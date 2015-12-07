7bbc16f09a708eb30ab81d3392f0b24c75e8d4b0
Earl Espino <ee760971@wcupa.edu>
2015-12-07 14:48:18 -0500

    Update day1.ml

--- day1.ml
+++ day1.ml
@@ -1,3 +1,6 @@
+val parens = TextIO.openIn "parens.txt";
+val parensLine = TextIO.inputLine parens;
+val explodedParens = (explode parensLine);
 fun day1 (nil, n) = n
   | day1 ((x::xs), ~1) = (length explodedParens) - (length (x::xs))
   | day1 ((#"("::xs), n) = part1(xs,n+1);
